b1369d9f20d026e7410a7b20b19c48847b78b514
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-09-23 01:23:27 +0900

    Update

--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.12
 
 require (
 	github.com/0xAX/notificator v0.0.0-20181105090803-d81462e38c21 // indirect
+	github.com/aclements/perflock v0.0.0-20180319185109-8402f33a418d // indirect
 	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
 	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
-	github.com/docker/docker v1.13.1
 	github.com/fsouza/go-dockerclient v1.4.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
 	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/graphql-go/graphql v0.7.8
+	github.com/graphql-go/handler v0.2.3
+	github.com/graphql-go/relay v0.0.0-20171208134043-54350098cfe5
 	github.com/json-iterator/go v1.1.7 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
 	github.com/mattn/go-shellwords v1.0.6 // indirect
@@ -18,7 +21,7 @@ require (
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/ugorji/go v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
-	golang.org/x/net v0.0.0-20190918130420-a8b05e9114ab // indirect
+	golang.org/x/net v0.0.0-20190918130420-a8b05e9114ab
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20190919044723-0c1ff786ef13 // indirect
 	golang.org/x/text v0.3.2 // indirect
--- main.go
+++ main.go
@@ -1,42 +1,285 @@
 package main
 
 import (
+	"bara/data"
 	"fmt"
-	"os/exec"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 )
 
-// SubmitCode has code users typed
-type SubmitCode struct {
-	TypedCode string `json:"typedCode"`
-	Lang      string `json:"lang"`
+type Product struct {
+	ID    int64   `json:"id"`
+	Name  string  `json:"name"`
+	Info  string  `json:"info,omitempty"`
+	Price float64 `json:"price"`
+}
+
+var products []Product
+
+var productType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Product",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"info": &graphql.Field{
+				Type: graphql.String,
+			},
+			"price": &graphql.Field{
+				Type: graphql.Float,
+			},
+		},
+	},
+)
+
+var queryType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			/* Get (read) single product by id
+			   http://localhost:8080/product?query={product(id:1){name,info,price}}
+			*/
+			"product": &graphql.Field{
+				Type:        productType,
+				Description: "Get product by id",
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.Int,
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					id, ok := p.Args["id"].(int)
+					if ok {
+						// Find product
+						for _, product := range products {
+							if int(product.ID) == id {
+								return product, nil
+							}
+						}
+					}
+					return nil, nil
+				},
+			},
+			/* Get (read) product list
+			   http://localhost:8080/product?query={list{id,name,info,price}}
+			*/
+			"list": &graphql.Field{
+				Type:        graphql.NewList(productType),
+				Description: "Get product list",
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					return products, nil
+				},
+			},
+		},
+	})
+
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		/* Create new product item
+		http://localhost:8080/product?query=mutation+_{create(name:"Inca Kola",info:"Inca Kola is a soft drink that was created in Peru in 1935 by British immigrant Joseph Robinson Lindley using lemon verbena (wiki)",price:1.99){id,name,info,price}}
+		*/
+		"create": &graphql.Field{
+			Type:        productType,
+			Description: "Create new product",
+			Args: graphql.FieldConfigArgument{
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+				"info": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"price": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Float),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				rand.Seed(time.Now().UnixNano())
+				product := Product{
+					ID:    int64(rand.Intn(100000)), // generate random ID
+					Name:  params.Args["name"].(string),
+					Info:  params.Args["info"].(string),
+					Price: params.Args["price"].(float64),
+				}
+				products = append(products, product)
+				return product, nil
+			},
+		},
+
+		/* Update product by id
+		   http://localhost:8080/product?query=mutation+_{update(id:1,price:3.95){id,name,info,price}}
+		*/
+		"update": &graphql.Field{
+			Type:        productType,
+			Description: "Update product by id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"info": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"price": &graphql.ArgumentConfig{
+					Type: graphql.Float,
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, _ := params.Args["id"].(int)
+				name, nameOk := params.Args["name"].(string)
+				info, infoOk := params.Args["info"].(string)
+				price, priceOk := params.Args["price"].(float64)
+				product := Product{}
+				for i, p := range products {
+					if int64(id) == p.ID {
+						if nameOk {
+							products[i].Name = name
+						}
+						if infoOk {
+							products[i].Info = info
+						}
+						if priceOk {
+							products[i].Price = price
+						}
+						product = products[i]
+						break
+					}
+				}
+				return product, nil
+			},
+		},
+
+		/* Delete product by id
+		   http://localhost:8080/product?query=mutation+_{delete(id:1){id,name,info,price}}
+		*/
+		"delete": &graphql.Field{
+			Type:        productType,
+			Description: "Delete product by id",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.Int),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				id, _ := params.Args["id"].(int)
+				product := Product{}
+				for i, p := range products {
+					if int64(id) == p.ID {
+						product = products[i]
+						// Remove from product list
+						products = append(products[:i], products[i+1:]...)
+					}
+				}
+
+				return product, nil
+			},
+		},
+	},
+})
+
+var schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query:    queryType,
+		Mutation: mutationType,
+	},
+)
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("errors: %v", result.Errors)
+	}
+	return result
+}
+
+func initProductsData(p *[]Product) {
+	product1 := Product{ID: 1, Name: "Chicha Morada", Info: "Chicha morada is a beverage originated in the Andean regions of Perú but is actually consumed at a national level (wiki)", Price: 7.99}
+	product2 := Product{ID: 2, Name: "Chicha de jora", Info: "Chicha de jora is a corn beer chicha prepared by germinating maize, extracting the malt sugars, boiling the wort, and fermenting it in large vessels (traditionally huge earthenware vats) for several days (wiki)", Price: 5.95}
+	product3 := Product{ID: 3, Name: "Pisco", Info: "Pisco is a colorless or yellowish-to-amber colored brandy produced in winemaking regions of Peru and Chile (wiki)", Price: 9.95}
+	*p = append(*p, product1, product2, product3)
 }
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	// Primary data initialization
+	// initProductsData(&products)
+
+	// http.HandleFunc("/product", func(w http.ResponseWriter, r *http.Request) {
+	// 	result := executeQuery(r.URL.Query().Get("query"), schema)
+	// 	json.NewEncoder(w).Encode(result)
+	// })
+
+	// fmt.Println("Server is running on port 8080")
+	// http.ListenAndServe(":8080", nil)
+
+	// simplest relay-compliant graphql server HTTP handler
+	h := handler.New(&handler.Config{
+		Schema: &data.Schema,
+		Pretty: true,
 	})
-	r.POST("/:id/interpret_solution", func(c *gin.Context) {
-		var submitCode SubmitCode
 
-		if err := c.BindJSON(&submitCode); err != nil {
-			return
-		}
+	// create graphql endpoint
+	http.Handle("/graphql", h)
 
-		inputCommand := fmt.Sprintf(`echo "%s" > ./temp && echo "%s" >> ./temp  && node temp`, submitCode.TypedCode, "helloworld()")
-		out, err := exec.Command("docker", "run", "node:12.10.0-alpine", "/bin/ash", "-c", inputCommand).Output()
-
-		if err != nil {
-			return
-		}
-
-		c.JSON(200, gin.H{
-			"result": string(out),
-		})
-	})
-	r.Run()
+	// serve!
+	port := ":8080"
+	log.Printf(`GraphQL server starting up on http://localhost%v`, port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.Fatalf("ListenAndServe failed, %v", err)
+	}
 }
+
+// import (
+// 	"fmt"
+// 	"os/exec"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// // SubmitCode has code users typed
+// type SubmitCode struct {
+// 	TypedCode string `json:"typedCode"`
+// 	Lang      string `json:"lang"`
+// }
+
+// func main() {
+// 	r := gin.Default()
+// 	r.GET("/ping", func(c *gin.Context) {
+// 		c.JSON(200, gin.H{
+// 			"message": "pong",
+// 		})
+// 	})
+// 	r.POST("/:id/interpret_solution", func(c *gin.Context) {
+// 		var submitCode SubmitCode
+
+// 		if err := c.BindJSON(&submitCode); err != nil {
+// 			return
+// 		}
+
+// 		inputCommand := fmt.Sprintf(`echo "%s" > ./temp && echo "%s" >> ./temp  && node temp`, submitCode.TypedCode, "helloworld()")
+// 		out, err := exec.Command("docker", "run", "node:12.10.0-alpine", "/bin/ash", "-c", inputCommand).Output()
+
+// 		if err != nil {
+// 			return
+// 		}
+
+// 		c.JSON(200, gin.H{
+// 			"result": string(out),
+// 		})
+// 	})
+// 	r.Run()
+// }
