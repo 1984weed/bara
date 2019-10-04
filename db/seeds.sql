f45e52d048575fe49d13184cba422f0b7cf16efe
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-10-04 19:35:56 +0900

    Update editor UI

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 INSERT INTO questions (slug, title, description, function_name, arg_id, language_id, author_id, created_at, updated_at)
     VALUES ('test-slug', 'test title', 'test-description you have to create a code', 'helloWorld', 1, 1, 1, now(), now());
 
-INSERT INTO question_args (question_id, order_no, name, type) VALUES
-    (2, 1, 'nums', 'num');
-INSERT INTO question_args (question_id, order_no, name, type) VALUES
-    (2, 2, 'target', 'num');
+INSERT INTO question_args (question_id, order_no, name, type, created_at, updated_at) VALUES
+    (1, 1, 'nums', 'num', now(), now());
+INSERT INTO question_args (question_id, order_no, name, type, created_at, updated_at) VALUES
+    (1, 2, 'target', 'num', now(), now());
 
-INSERT INTO question_testcases (question_id, input_text, output_text) VALUES
-    (2, '[2,7,11,15]\n9', '[0,1]' );
-INSERT INTO question_testcases (question_id, input_text, output_text) VALUES
-    (2, '[3,2,4]\n6', '[1,2]');
+INSERT INTO question_testcases (question_id, input_text, output_text, created_at, updated_at) VALUES
+    (1, '[2,7,11,15]\n9', '[0,1]', now(), now());
+INSERT INTO question_testcases (question_id, input_text, output_text, created_at, updated_at) VALUES
+    (1, '[3,2,4]\n6', '[1,2]', now(), now());
--- front-bara/pages/problems/[slug].tsx
+++ front-bara/pages/problems/[slug].tsx
@@ -4,6 +4,7 @@ import Editor from '../../components/Editor'
 import React from 'react'
 import { useQuery } from 'graphql-hooks'
 import {Question} from '../../graphql/types'
+import { Grid, Button, Box } from 'grommet';
 
 type Props = {}
 
@@ -11,6 +12,7 @@ const problem = `
 query getQuestion($slug: String!) {
     Question(slug: $slug) {
         title,
+        description,
         slug,
         codeSnippets {
             lang,
@@ -28,24 +30,47 @@ const Problem: React.FunctionComponent<Props> = ({}: Props) => {
         variables: { slug },
         updateData: (_, result) => ({
           ...result
-        //   allPosts: [...prevResult.allPosts, ...result.allPosts]
         })
       })
 
     if (error) return <span>Error</span>
     if (!data) return <div>Loading</div>
 
-    console.log(data)
     const { Question } = data
     const targetCodeSnippet = Question.codeSnippets.find(a => a.lang === "JavaScript") || {code: ""};
-    console.log(targetCodeSnippet)
-    
+
     return (
         <Layout title="">
-            <h1>{slug}</h1>
-            <Editor
-                value={targetCodeSnippet.code}
-            />
+            <Grid
+            rows={['flex', '60px']}
+            columns={['flex', '5px', 'flex']}
+            gap='1px'
+            areas={[
+            { name: 'description', start: [0, 0], end: [0, 0] },
+            { name: 'partition', start: [1, 0], end: [1, 0] },
+            { name: 'editor', start: [2, 0], end: [2, 0] },
+            { name: 'controls', start: [0, 1], end: [2, 1] },
+            ]}
+        >
+                <Box gridArea='description'>
+                    <h1>{Question.title}</h1>
+                    <Box>
+                        {Question.description}
+                    </Box>
+                </Box>
+                <div></div>
+                <Box gridArea='editor'>
+                    <Editor
+                        value={targetCodeSnippet.code}
+                    />
+                </Box>
+                <Box gridArea='controls'>
+                    <Box direction="row" justify="end" margin={{ top: "medium" }}>
+                        <Button label="Cancel" />
+                        <Button type="submit" label="Submit" primary />
+                    </Box>
+                </Box>
+            </Grid>
         </Layout>
     )
 }
